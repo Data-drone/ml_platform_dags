4ff47b92682dad14d5102f94193b6c5a91f31d5f
Data-drone <bpl.law@gmail.com>
2021-06-11 17:15:25 +1000

    updated to use the external metastore

--- projects/taxi_load/modules/delta_ingest.py
+++ projects/taxi_load/modules/delta_ingest.py
@@ -8,16 +8,21 @@ os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages {0} pyspark-shell".format(packag
     
 spark = SparkSession \
     .builder \
-    .appName("Load Data") \
+    .appName("Jupyter") \
     .config("spark.master", "spark://spark-master:7077") \
     .config("spark.num.executors", "2") \
+    .config("spark.executor.memory", "8G") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
     .config("spark.hadoop.fs.s3a.access.key", os.environ['MINIO_ACCESS_KEY']) \
     .config("spark.hadoop.fs.s3a.secret.key", os.environ['MINIO_SECRET_KEY']) \
-    .config("spark.hadoop.fs.s3a.endpoint", "172.30.0.4:9000") \
+    .config("spark.hadoop.fs.s3a.endpoint", "minio:9000") \
     .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
     .config("spark.hadoop.fs.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.metastore.catalog.default", "hive") \
+    .config("spark.sql.warehouse.dir", "s3a://storage/warehouse") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.connection.maximum", "50") \
     .enableHiveSupport() \
     .getOrCreate()
 
@@ -26,4 +31,4 @@ green_trip_2015_test = spark.read.option("header", True).csv(green_trip_data_201
 
 green_trip_2015_test = green_trip_2015_test.withColumnRenamed("Trip_type ", "trip_type")
 
-green_trip_2015_test.write.format("delta").mode('overwrite').save("s3a://storage/warehouse/green_taxi_2015_test")
+green_trip_2015_test.write.format("delta").mode("overwrite").saveAsTable("green_taxi_2015_table")
